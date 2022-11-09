099cc5eccb9c99deae36748637da57d25ddddf1f
KathaWei <katharina.w99@web.de>
2022-11-09 10:23:53 +0100

    removed log install and build folder with files

--- build/edu_simulation/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/daniel/ros2_ws/src/edu_simulation
-# Build directory: /home/daniel/ros2_ws/src/edu_simulation/build/edu_simulation
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
